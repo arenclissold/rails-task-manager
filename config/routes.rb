09dd195653cad2dd11ccf236b60edf18b1152baf
Aren Clissold <arenclissold@gmail.com>
2022-05-24 14:36:08 +1000

    CRUD added to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  get 'task/:id', to: 'tasks#show', as: 'task'
+  patch 'task/:id', to: 'tasks#update'
+  get 'task/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  delete 'task/:id', to: 'tasks#destroy'
 end
