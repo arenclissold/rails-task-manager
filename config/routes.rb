c5578e88416dfae97ff9e856a7b02b6d443443ba
Aren Clissold <arenclissold@gmail.com>
2022-05-24 16:49:55 +1000

    made task view look better

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/new', to: 'tasks#new'
-  get 'task/:id', to: 'tasks#show', as: 'task'
-  patch 'task/:id', to: 'tasks#update'
-  get 'task/:id/edit', to: 'tasks#edit', as: 'task_edit'
-  delete 'task/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'task/:id', to: 'tasks#show', as: 'task'
+  # patch 'task/:id', to: 'tasks#update'
+  # get 'task/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  # delete 'task/:id', to: 'tasks#destroy'
+  resources :tasks
+  patch 'task/:id/check', to: 'tasks#check', as: 'check_task'
 end
